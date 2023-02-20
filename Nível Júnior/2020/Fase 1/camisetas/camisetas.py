65f95a6ab4a47d0147f33cc8dccb9001998148d6
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-02-20 12:00:02 -0300

    Camisetas

---
+++ Nível Júnior/2020/Fase 1/camisetas/camisetas.py
@@ -0,0 +1,22 @@
+#Premiados
+N = int(input())
+
+#Array com tamanhos escolhidos
+T = [int(i) for i in input().split()]
+
+#Contando os escolhidos de cada tamanho
+escolhidos1 = T.count(1)
+escolhidos2 = T.count(2)
+
+#Camisetas pequenas produzidas
+P = int(input())
+
+#Camisetas médias produzidas
+M = int(input())
+
+if P >= escolhidos1 and M >= escolhidos2:
+    print("S")
+else:
+    print("N")
+
+#By Daniel Dias Pereira
