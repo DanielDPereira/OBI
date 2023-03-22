8b4355aa9b6eb08d83ba0092fc870582eb8ee74c
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-03-21 21:28:03 -0300

    Update codigo.py

--- Nível Júnior/2015/Fase 2/codigo.py
+++ Nível Júnior/2015/Fase 2/codigo.py
@@ -2,5 +2,14 @@ N = int(input())
 sequencia = input()
 
 sequencia_list = sequencia.split(" ")
+
+X = 0
+
+for i in range(len(sequencia_list)-2):    
+    if sequencia_list[i] == "1":
+        if sequencia_list[i+1] == "0":
+            if sequencia_list[i+2] == "0":
+                X = X + 1
     
 print(sequencia_list)
+print(X)
