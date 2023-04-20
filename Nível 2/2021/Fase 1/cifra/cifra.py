34373634d263096621e5e588de067bc6618a1c46
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-20 17:28:56 -0300

    Update cifra.py

--- Nível 2/2021/Fase 1/cifra/cifra.py
+++ Nível 2/2021/Fase 1/cifra/cifra.py
@@ -2,3 +2,4 @@ P = input()
 
 alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "z"]
 vogais = ["a", "e", "i", "o", "u"]
+consoantes = []
